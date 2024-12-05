357f570466736d8a7343aa0c52bdfed9e0a7d641
“vignesh3447” <vigneshwaran3447@gmail.com>
2024-12-05 20:24:20 +0530

    chang

--- missing_array.c
+++ missing_array.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    int n,arr[]={1,2,3,4,5,7,8,9,10};
+    int arr[]={1,2,3,4,5,7,8,9,10};
+    int len=sizeof(arr)/sizeof(arr[0]);
+    int n;
 
-    for (int i=0; i<(sizeof(arr)/sizeof(arr[0])); i++)
+    for (int i=0; i<len; i++)
     {
         n=(arr[i=1]-arr[i]);
         for (int j=1; j<n; j++)
@@ -12,9 +14,6 @@ int main()
             printf("%d \n",arr[i]+j);
         }
 
-
-
     }
-
     return 0;
 }
